d612518c04e14eb68e6ab4bdb48697c5687fb162
Osmankaankorkmaz <osmankaankorkmaz921@gmail.com>
2024-10-23 14:25:41 +0300

    site güncellendi

--- src/component/TipsCard.jsx
+++ src/component/TipsCard.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 const TipsCard = ({ title, description }) => {
   return (
-    <div className="w-full max-w-xs h-60 bg-[#6c757d] rounded-lg overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105 text-white mx-auto cursor-default ">
-      <div className="pt-2 pb-2 px-4 text-center h-full flex flex-col justify-center">
-        <h3 className="text-lg sm:text-base md:text-lg font-bold mb-10">{title}</h3>
-        <div className="text-sm leading-relaxed break-words h-24 flex items-center justify-center">
-          <p>{description}</p>
+    <div className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg h-60 bg-[#6c757d] rounded-lg overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105 text-white mx-auto cursor-default">
+      <div className="pt-4 pb-4 px-6 text-center h-full flex flex-col justify-center">
+        <h3 className="text-xl sm:text-2xl md:text-3xl font-bold mb-4">{title}</h3>
+        <div className="text-sm sm:text-base md:text-lg leading-relaxed break-words flex-1 overflow-hidden">
+          <div className="">
+            <p>{description}</p>
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default TipsCard;
